cd5f1b10178290cf4494cc58d057ecefb8760360
hwfhc <gyczt879574764@gmail.com>
2018-09-18 21:59:59 +0800

    debug

--- config/index.js
+++ config/index.js
@@ -11,14 +11,14 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/': {
-        target: 'http://127.0.0.1:8081',
+        target: 'http://127.0.0.1:8080',
         changeOrigin: true
       }
     },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
-    port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    port: 80, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: true,
